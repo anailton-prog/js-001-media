2019226b5da32bb3cbb5500f74f45813121a9da5
anailton <jesusanailton706@gmail.com>
2025-05-09 22:29:12 -0300

    desafio

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
 let n1 = 7, n2 = 5.5, n3 = 2, n4 = 4
 let média = (n1 + n2 + n3 + n4) / 4
 console.log("a média é: " + média.toFixed(2))
+
+if(média>=5){
+    console.log("parabens, você passou")
+}else{
+    console.log("que pena, volte na proxima")
+}
